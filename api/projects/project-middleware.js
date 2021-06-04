13ce5521fe7225d0989193db9e6cf2eaf197e23e
Bruno Paula <devbrunopaula@gmail.com>
2021-06-04 12:21:29 -0400

    projects GetBy ID completed

--- api/projects/project-middleware.js
+++ api/projects/project-middleware.js
@@ -2,9 +2,22 @@ const Project = require('./projects-model')
 
 class Validade {
   projectId = async (req, res, next) => {
-    const {id} = req.params
     try {
-    } catch (error) {}
+      const data = await Project.get(req.params.id)
+
+      if (!data) {
+        next({status: 404, message: 'Project Not Found!'})
+      } else {
+        req.project = data
+        next()
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  validateEntries = (req, res, next) => {
+    const {name, description, completed} = req.body
   }
 }
 
