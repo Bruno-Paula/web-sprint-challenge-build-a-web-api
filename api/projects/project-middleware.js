9a73ce6614aef17b48d4e76aeb988b0ef909ca96
Bruno Paula <devbrunopaula@gmail.com>
2021-06-04 11:55:46 -0400

    get projects completed

---
+++ api/projects/project-middleware.js
@@ -0,0 +1,11 @@
+const Project = require('./projects-model')
+
+class Validade {
+  projectId = async (req, res, next) => {
+    const {id} = req.params
+    try {
+    } catch (error) {}
+  }
+}
+
+module.exports = new Validade()
